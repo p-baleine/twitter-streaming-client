ebd2919c56e01cddf683f5a5f2abac0e0b7dec90
Tajima Junpei <p.baleine@gmail.com>
2015-07-18 07:12:32 +0900

    modify publis stream url

--- src/twitter-streaming-client.js
+++ src/twitter-streaming-client.js
@@ -2,7 +2,7 @@ import ReconnectingProxy from "./reconnecting-proxy";
 import TwitterStreamClient from "./client";
 
 const USER_STREAM_ENDPOINT = "https://userstream.twitter.com/1.1/user.json";
-const PUBLIC_STREAM_ENDPOINT = "git@github.com:p-baleine/twitter-streaming-client.git";
+const PUBLIC_STREAM_ENDPOINT = "https://stream.twitter.com/1.1/statuses/filter.json";
 
 export function userStream(oauth) {
   var client = new TwitterStreamClient(USER_STREAM_ENDPOINT, oauth, {});
