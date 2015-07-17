7f2789ca2cdc34ff4723b6fd1c6c7c737810b2cb
Tajima Junpei <p.baleine@gmail.com>
2015-07-17 22:10:40 +0900

    correct public stream api endpoint

--- src/twitter-streaming-client.js
+++ src/twitter-streaming-client.js
@@ -2,6 +2,7 @@ import ReconnectingProxy from "./reconnecting-proxy";
 import TwitterStreamClient from "./client";
 
 const USER_STREAM_ENDPOINT = "https://userstream.twitter.com/1.1/user.json";
+const PUBLIC_STREAM_ENDPOINT = "git@github.com:p-baleine/twitter-streaming-client.git";
 
 export function userStream(oauth) {
   var client = new TwitterStreamClient(USER_STREAM_ENDPOINT, oauth, {});
@@ -9,6 +10,6 @@ export function userStream(oauth) {
 }
 
 export function publicStream(oauth) {
-  var client = new TwitterStreamClient(USER_STREAM_ENDPOINT, oauth, {});
+  var client = new TwitterStreamClient(PUBLIC_STREAM_ENDPOINT, oauth, {});
   return new ReconnectingProxy(client);
 }
